8cdd8c9332e96ae3f066640dd9c02295007b0024
vaitiaredr <ninaerikmimi@gmail.com>
2024-01-16 09:27:15 +0100

    cambios

---
+++ main.js
@@ -0,0 +1,6 @@
+function search (){
+    val, searchTerm = document.getElementById("searchInput").value,
+    val, resultsContainer = document.getElementById("results"),
+    resultsContainer,innerHTML = "<p>Resultados de la búsqueda para: " + searchTerm + "</p>"
+};
+console.log("nineta")
